037fcd4b5dd83778de590ae3eb955a9850025e07
Danil <danila_monster@mail,ru>
2022-09-24 14:04:47 +0300

    Task06

--- Task_05.py
+++ Task_05.py
@@ -13,7 +13,8 @@
 
 num = int(input('Введите число: '))
 
-if (num % 5 == 0 and num % 10 == 0 or num % 15 == 0) and num % 30 !=0:
+if (num % 5 == 0 and num % 10 == 0 or num % 15 == 0) and num % 30 != 0:
     print("Yes")
 else:
     print("No")
+
